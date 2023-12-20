11b7f7d4b7fa74de4f91f3f4048a69c6e345a7a5
Fatima Binte Tanveer <fatimabintetanveer@gmail.com>
2023-12-20 13:48:42 +0500

    Add specifications for methods

--- generic_helper.py
+++ generic_helper.py
@@ -1,11 +1,31 @@
 import re
 
 def get_str_from_food_dict(food_dict: dict):
+    
+    """
+    Converts a dictionary of food items and quantities into a string.
+
+    Parameters:
+        - food_dict (dict): Dictionary containing food items as keys and quantities as values.
+
+    Returns:
+        - str: A string representation of the food items and quantities.
+    """
+
     result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
     return result
 
 
 def extract_session_id(session_str: str):
+    """
+    Extracts the session ID from a string containing a session context.
+
+    Parameters:
+        - session_str (str): String containing the session context.
+
+    Returns:
+        - str: Extracted session ID.
+    """
     match = re.search(r"/sessions/(.*?)/contexts/", session_str)
     if match:
         extracted_string = match.group(1)
